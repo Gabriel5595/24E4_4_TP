6eb4c203b974bce7dfb5c30ec2aeb494f3155146
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2024-11-10 22:15:11 +0100

    TPs1.15-16: Instalação da biblioteca MUI Material e teste com adição de componente Button na página Sign In

--- src/views/SignIn.tsx
+++ src/views/SignIn.tsx
@@ -1,7 +1,11 @@
+import ButtonComponent from "../components/button"
+
 const SignIn = () => {
     return (
         <div>
             <h1>Sign In</h1>
+
+            <ButtonComponent>Botão</ButtonComponent>
         </div>
     )
 }
