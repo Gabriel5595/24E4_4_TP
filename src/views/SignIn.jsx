87acc7fac58726f9d7b3a0343014f6e2c496e218
Gabriel Carvalho <gabriel.vcarvalho@al.infnet.edu.br>
2024-11-26 17:57:20 +0100

    TP2.4: Criação do componente Card e teste na página SingIn

--- src/views/SignIn.jsx
+++ src/views/SignIn.jsx
@@ -2,6 +2,7 @@ import AlertComponent from "../components/alert"
 import AvatarComponent from "../components/avatar"
 import ButtonComponent from "../components/button"
 import BoxComponent from "../components/box"
+import CardComponent from "../components/card"
 
 const SignIn = () => {
     return (
@@ -16,6 +17,7 @@ const SignIn = () => {
                 alt='Master Chief' 
                 src='https://imgcdn.stablediffusionweb.com/2024/5/25/092519db-ab58-4ae2-9954-526e1626d629.jpg' 
                 sx={{
+                    margin: 5,
                     width: 90,
                     height: 90,
                 }}
@@ -26,12 +28,20 @@ const SignIn = () => {
                 sx={{
                     p:2,
                     border: '1px dashed grey',
-                    magin:5
                 }}>
                     This is a box.
             </BoxComponent>
 
-            
+            <CardComponent 
+                variant='outlined'
+                sx={{
+                    margin: 10,
+                    maxWidth: 300,
+                    height: 200
+                }}
+            >
+                Este é um cartão
+            </CardComponent>
         </div>
     )
 }
