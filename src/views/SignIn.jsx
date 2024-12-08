cf7b12c7ceb5d483fd8228804d00d8bfd815d90b
Gabriel Carvalho <gabriel.vcarvalho@al.infnet.edu.br>
2024-12-08 11:51:03 +0100

    TP3.2: Construa a tela SignIn do seu projeto

--- src/views/SignIn.jsx
+++ src/views/SignIn.jsx
@@ -1,9 +1,58 @@
+import ContainerComponent from "../components/container";
+import TypographyComponent from "../components/typography";
+import TextFieldComponent from "../components/textField";
+import ButtonComponent from "../components/button";
+import BoxComponent from "../components/box";
+
 const SignIn = () => {
     return (
-        <div>
-            <h1>Sign In</h1>
-        </div>
-    )
-}
+        <ContainerComponent
+            maxWidth="xs"
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                marginTop: "50px",
+                padding: "20px",
+            }}
+        >
+            <TypographyComponent variant="h4" style={{ marginBottom: "20px" }}>
+                Sign In
+            </TypographyComponent>
 
-export default SignIn
+            <BoxComponent
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "15px",
+                    width: "100%",
+                }}
+            >
+                <TextFieldComponent label="Email" variant="outlined" fullWidth />
+                <TextFieldComponent
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    fullWidth
+                />
+            </BoxComponent>
+
+            <ButtonComponent
+                variant="contained"
+                color="primary"
+                style={{ marginTop: "30px", width: "100%" }}
+            >
+                Login
+            </ButtonComponent>
+
+            <TypographyComponent
+                variant="body2"
+                style={{ marginTop: "15px", color: "#007BFF", cursor: "pointer" }}
+            >
+                Forgot your password?
+            </TypographyComponent>
+        </ContainerComponent>
+    );
+};
+
+export default SignIn;
